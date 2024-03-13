e603f6e26312eed73e47441a760af4a0cf8fa9eb
DatPham124 <phof1242003@gmail.com>
2024-03-13 17:39:10 +0700

    Header va footer

---
+++ public/index.php
@@ -0,0 +1,17 @@
+<?php
+    include_once __DIR__ . '/../src/partials/header.php';
+    include_once __DIR__ . '/../src/partials/heading.php';
+   
+?>
+
+<body>
+    
+
+
+</div>
+</body>
+
+<?php
+ include_once __DIR__ . '/../src/partials/footer.php';
+?>
+
